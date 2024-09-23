f8162c370f5dc1a9f0ac6a43d05d038cb3b015c9
Joe Abbate <josephabbateny@gmail.com>
2024-09-23 10:38:24 -0400

    Secure Cookie

--- src/main.rs
+++ src/main.rs
@@ -91,7 +91,7 @@ async fn main() -> std::io::Result<()> {
             }))
             .wrap(
                 SessionMiddleware::builder(CookieSessionStore::default(), session_key.clone())
-                    .cookie_secure(false)
+                    .cookie_secure(!env::var("DEVELOPMENT").is_ok())
                     .build(),
             )
             .wrap(Logger::default())
